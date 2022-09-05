7bd4901c3f05ad296cb719a9c62a1516c1f0acf1
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-09-05 08:38:48 +0530

    Update swapAlternate.cpp

--- swapAlternate.cpp
+++ swapAlternate.cpp
@@ -29,3 +29,45 @@ std::cout<<arr[i]<<" ";
 }
 
 }
+
+
+// Java code:
+
+
+import java.util.*;
+
+public class swapAlternate{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int arr[]=new int[size];
+    for(int i=0;i<size;i++){
+    arr[i]=sc.nextInt();
+    }
+    int temp;
+    int i=0;
+    while(i<arr.length-1){
+    temp=arr[i];
+    arr[i]=arr[i+1];
+    arr[i+1]=temp;
+    i=i+2;
+    }
+    for(int i=0;i<size;i++){
+    System.out.print(arr[i]+" ");
+    }
+  }
+}
+
+
+//using recursion:
+
+int swapAlternate(int *arr, int size){
+  int temp, i=0;
+if(!(size<2){
+ temp=arr[0];
+  arr[0]=arr[i+1];
+  arr[i+1]=temp;
+  i=i+2;
+  swapAlternate(arr+2, size-2);
+}
+}
